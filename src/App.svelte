30ca800f1bb4dfcbf47e5607c1ca9382ff5bf126
HuskyHsu <schi.hsu@gmail.com>
2021-05-26 23:43:03 +0800

    update template

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
 	import Tailwindcss from "./Tailwindcss.svelte";
+
+	let chapters = [
+		{
+			title: "圖文滿版區塊",
+			imgUrl: "https://i.imgur.com/B8D7A3d.jpg",
+		},
+		{
+			title: "製作一個互動圖文卡片",
+			imgUrl: "https://i.imgur.com/O1jK6Op.jpg",
+		},
+		{
+			title: "人員介紹卡片",
+			imgUrl: "https://i.imgur.com/td9zZ21.jpg",
+		},
+		{
+			title: "交錯漂浮版",
+			imgUrl: "https://i.imgur.com/n3ztRBl.jpg",
+		},
+	];
 </script>
 
 <Tailwindcss />
@@ -19,49 +38,23 @@
 	</nav>
 	<main class="py-4">
 		<div class="px-4">
-			<div class="block md:flex md:-mx-2">
-				<div
-					class="h-[250px] w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0 sm:w-96 relative bg-green-100"
-				>
-					<img
-						class="h-full object-cover w-full"
-						src="https://i.imgur.com/B8D7A3d.jpg"
-						alt=""
-					/>
-					<p
-						class="absolute bottom-4 bg-white bg-opacity-95 shadow-md rounded-r-full p-3 font-bold text-xs"
+			<div class="block md:flex md:-mx-2 md:flex-wrap">
+				{#each chapters as chapter, i}
+					<div
+						class="h-[250px] w-full lg:w-1/3 md:px-2 mb-4 md:mb-4 sm:w-96 relative"
 					>
-						01-圖文滿版區塊
-					</p>
-				</div>
-				<div
-					class="h-[250px] w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0 sm:w-96 relative bg-green-100"
-				>
-					<img
-						class="h-full object-cover w-full"
-						src="https://i.imgur.com/O1jK6Op.jpg"
-						alt=""
-					/>
-					<p
-						class="absolute bottom-4 bg-white bg-opacity-95 shadow-md rounded-r-full p-3 font-bold text-xs"
-					>
-						02-製作一個互動圖文卡片
-					</p>
-				</div>
-				<div
-					class="h-[250px] w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0 sm:w-96 relative bg-green-100"
-				>
-					<img
-						class="h-full object-cover w-full"
-						src="https://i.imgur.com/td9zZ21.jpg"
-						alt=""
-					/>
-					<p
-						class="absolute bottom-4 bg-white bg-opacity-95 shadow-md rounded-r-full p-3 font-bold text-xs"
-					>
-						03-人員介紹卡片
-					</p>
-				</div>
+						<img
+							class="h-full object-cover w-full shadow-lg"
+							src={chapter.imgUrl}
+							alt=""
+						/>
+						<p
+							class="absolute bottom-4 bg-white bg-opacity-95 shadow-md rounded-r-full p-3 font-bold text-xs"
+						>
+							{`${i > 9 ? "" : "0"}${i + 1}-${chapter.title}`}
+						</p>
+					</div>
+				{/each}
 			</div>
 		</div>
 	</main>
